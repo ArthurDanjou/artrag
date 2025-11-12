758fe2bc664956fa6b9f3f36f74ebec013d2757f
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-11-12 23:36:17 +0100

    refactor: simplifier les descriptions en texte brut et ajouter des fichiers Markdown pour les catégories

--- server/api/uses.get.ts
+++ server/api/uses.get.ts
@@ -1,3 +1,10 @@
-export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+import { queryCollection } from '@nuxt/content/server'
+
+export default defineCachedEventHandler(async (event) => {
+  return await queryCollection(event, 'uses')
+    .where('extension', '=', 'md')
+    .all()
+}, {
+  name: 'uses-list',
+  maxAge: 3600 // 1 hour
 })
