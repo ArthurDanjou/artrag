8c7c9d289d2940679b6abf7e81f268d6af98715d
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-11-13 14:04:15 +0100

    refactor: remplacer les gestionnaires d'événements par des gestionnaires d'événements mis en cache pour les compétences et les usages

--- server/api/skills.get.ts
+++ server/api/skills.get.ts
@@ -1,3 +1,12 @@
-export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+import { queryCollection } from '@nuxt/content/server'
+
+export default defineCachedEventHandler(async (event) => {
+  return {
+    body: await queryCollection(event, 'skills')
+      .where('extension', '=', 'json')
+      .all()
+  }
+}, {
+  name: 'skills-list',
+  maxAge: 3600 // 1 hour
 })
--- server/api/uses.get.ts
+++ server/api/uses.get.ts
@@ -1,10 +1,22 @@
 import { queryCollection } from '@nuxt/content/server'
 
 export default defineCachedEventHandler(async (event) => {
+  const categories = await queryCollection(event, 'usesCategories').where('extension', '=', 'md').all()
+
+  const uses = await queryCollection(event, 'uses')
+    .where('extension', '=', 'md')
+    .all()
+
+  const uses_by_categories = []
+  for (const category of categories) {
+    uses_by_categories.push({
+      category: category,
+      uses: uses.filter(use => use.category === category.slug)
+    })
+  }
+
   return {
-    body: await queryCollection(event, 'uses')
-      .where('extension', '=', 'md')
-      .all()
+    body: uses_by_categories
   }
 }, {
   name: 'uses-list',
