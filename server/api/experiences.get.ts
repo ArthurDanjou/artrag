0539f2b5646181358ebfd5e40c749d421acf3adc
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-11-13 13:57:50 +0100

    refactor: encapsuler les résultats des requêtes dans un objet pour une meilleure structure des réponses

--- server/api/experiences.get.ts
+++ server/api/experiences.get.ts
@@ -1,9 +1,11 @@
 import { queryCollection } from '@nuxt/content/server'
 
 export default defineCachedEventHandler(async (event) => {
-  return await queryCollection(event, 'experiences')
-    .where('extension', '=', 'md')
-    .all()
+  return {
+    body: await queryCollection(event, 'experiences')
+      .where('extension', '=', 'md')
+      .all()
+  }
 }, {
   name: 'experiences-list',
   maxAge: 3600 // 1 hour
--- server/api/uses.get.ts
+++ server/api/uses.get.ts
@@ -1,9 +1,11 @@
 import { queryCollection } from '@nuxt/content/server'
 
 export default defineCachedEventHandler(async (event) => {
-  return await queryCollection(event, 'uses')
-    .where('extension', '=', 'md')
-    .all()
+  return {
+    body: await queryCollection(event, 'uses')
+      .where('extension', '=', 'md')
+      .all()
+  }
 }, {
   name: 'uses-list',
   maxAge: 3600 // 1 hour
